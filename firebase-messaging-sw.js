fa3fe58de8cca64a10cec36c9980a6fbbf181997
cennode <cennode@gmail.com>
2026-07-17 14:18:21 +0900

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,15 +1,18 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
+
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "umbrella-e6536.firebaseapp.com",
-    databaseURL: "https://umbrella-e6536-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "umbrella-e6536",
-    storageBucket: "umbrella-e6536.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC_jt0uVlyqctfNlcY5YAgr8jKEHIAQeak",
+  authDomain: "umbrella-e6536.firebaseapp.com",
+  databaseURL: "https://umbrella-e6536-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "umbrella-e6536",
+  storageBucket: "umbrella-e6536.firebasestorage.app",
+  messagingSenderId: "603025911927",
+  appId: "1:603025911927:web:a766e9e53089097cf11672",
+  measurementId: "G-0R09Q6SP56"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
